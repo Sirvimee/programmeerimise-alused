82b899ea3c31aa390cfaacbcbc268d6718cc0aeb
Meeli Sirvi <95487048+Sirvimee@users.noreply.github.com>
2022-01-07 08:58:28 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,11 +1,13 @@
 let a = 0
 function checkTemp(a) {
-    if (a<20 ){
+    if (a<=20 ){
     return -1
-    } else if (a>=20 && a<=40){
+    } else if (a>20 && a<=40){
     return 0
     }
     else if (a>=41 && a<=60)
     return 1
 }
-checkTemp(50)
+console.log (checkTemp(20))
+console.log (checkTemp(21))
+console.log (checkTemp(42))
